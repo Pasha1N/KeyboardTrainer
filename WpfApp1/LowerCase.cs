b32ddca22ab87af624f568ee2da8200df2fffb9f
Паша <@DESKTOP-3C2NDOK>
2018-08-13 20:37:48 +0300

    Close #10.

--- WpfApp1/LowerCase.cs
+++ WpfApp1/LowerCase.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KeyboardSimulator
+﻿namespace KeyboardSimulator
 {
-    static internal class LowerCase
+    internal static class LowerCase
     {
-        static private bool lowerCase = true;
+        private static bool lowerCase = true;
 
-        static public bool Register
+        public static bool Register
         {
             get => lowerCase;
             set => lowerCase = value;
